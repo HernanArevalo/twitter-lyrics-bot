f37a639e31a77bc7f2a739f630f6d58bc32e39cf
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-04-24 19:02:20 -0300

    first commit

---
+++ twitter-bot.py
@@ -0,0 +1,35 @@
+import os
+import tweepy
+import random
+import json
+
+
+
+API_KEY = os.environ[""]
+API_SECRET = os.environ[""]
+BEARER_TOKEN = os.environ[""]
+ACCESS_TOKEN = os.environ[""]
+ACCESS_SECRET = os.environ[""]
+
+
+
+# Gainaing access and connecting to Twitter API using Credentials
+    # client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+
+# Creating API instance. This is so we still have access to Twitter API V1 features
+    # auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+    # api = tweepy.API(auth)
+
+# Creating a tweet to test the bot
+    # client.create_tweet(text="Tenés la receta justa para hacerme sonreír.")
+    
+
+with open("phrases.json", "r") as f:
+    phrases = json.load(f)
+
+
+
+random_index = random.randint(0,len(phrases)-1)
+
+
+print(phrases[random_index])
