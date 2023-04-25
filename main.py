8df94d06ffd41ced5d2d54b6f0ee8afeedc3bfc2
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-04-25 14:03:23 -0300

    first commit

--- main.py
+++ main.py
@@ -14,22 +14,22 @@ ACCESS_SECRET = os.environ["ACCESS_SECRET"]
 
 
 # Gainaing access and connecting to Twitter API using Credentials
-    # client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
 # Creating API instance. This is so we still have access to Twitter API V1 features
-    # auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
-    # api = tweepy.API(auth)
+auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
 
-# Creating a tweet to test the bot
-    # client.create_tweet(text="Tenés la receta justa para hacerme sonreír.")
-    
 
 with open("phrases.json", "r") as f:
     phrases = json.load(f)
 
-
-
 random_index = random.randint(0,len(phrases)-1)
 
 
-print(phrases[random_index])
+
+# Creating a tweet to test the bot
+client.create_tweet(text=phrases[random_index])
+    
+
+#print(phrases[random_index])
