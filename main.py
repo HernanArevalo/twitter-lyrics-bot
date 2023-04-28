0bdb759c3100b4d75a4615851f7c1a938c526dc8
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-04-28 17:14:37 -0300

    first commit

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import random
 import json
 
 
+#API_KEY = os.environ["API_KEY"]
+#API_SECRET = os.environ["API_SECRET"]
+#BEARER_TOKEN = os.environ["BEARER_TOKEN"]
+#ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+#ACCESS_SECRET = os.environ["ACCESS_SECRET"]
 
-API_KEY = os.environ["API_KEY"]
-API_SECRET = os.environ["API_SECRET"]
-BEARER_TOKEN = os.environ["BEARER_TOKEN"]
-ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-ACCESS_SECRET = os.environ["ACCESS_SECRET"]
+API_KEY = "yzgp6Euh78sTP3SjayLxNSjWh"
+API_SECRET = "z0pmKKbjfIqhV72ysklI8mclx4wWJTR098llcLdpnvroPT9KMM"
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAMKSmwEAAAAAxBNpD8X4kTOi7gq2%2FrKgm5j%2BApw%3DxGwYu8UWaRTyelAZaIYtz5AhGcxPRoKspI62DuNvAQSq6IBRLO"
+ACCESS_TOKEN = "1181216688-yoKKKfE32aCnHGzpZIWSwOFmpiKNgP0FdlwjBZK"
+ACCESS_SECRET = "ZG3Ay4hvK7JxU3MAWQHOh4Yb3QGWIjw9N828c9mtzjYVp"
 
 
 
@@ -32,4 +37,4 @@ random_index = random.randint(0,len(phrases)-1)
 client.create_tweet(text=phrases[random_index])
     
 
-#print(phrases[random_index])
+print(phrases[random_index])
